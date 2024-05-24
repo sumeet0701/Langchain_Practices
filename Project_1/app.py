da3bbeba7bd68072cb5d028bdc7b4fb99140c2d5
sumeet <maheshwarisumeet@gmail.com>
2024-05-24 13:23:19 +0530

    Code pushed

---
+++ Project_1/app.py
@@ -0,0 +1,7 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
